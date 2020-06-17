ec6bd0d75a5a670bbac33c4cf016bf5307ba151f
Danilo Lutz <danilolutz@protonmail.com>
2020-06-16 22:03:41 -0300

    Challenge 2 commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Repositories List Backend
+
+[![MIT](https://img.shields.io/github/license/danilolutz/desafios-conceitos-node)](https://img.shields.io/github/license/danilolutz/desafios-conceitos-node)
+
+## :rocket: Overview
+
+First challenge of Rocketseat GoStack
+
+## :sunglasses: Get Started
+
+To run this projetct just download or clone it and then on root folder execute:
+
+```bash
+yarn
+```
+
+> **NOTE**: This will install all dependencies.
+
+### Running the project
+
+To run the project just execute:
+
+```bash
+yarn dev
+```
+
+And access on <http://localhost:3333>
+
+### Testing the project
+
+To test the project just execute:
+
+```bash
+yarn test
+```
+
+## :scroll: License
+
+This project is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+---
+
+Made with :purple_heart: by [Danilo Lutz](https://www.linkedin.com/in/danilolutz/)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
-
+import React, {useState,useEffect} from "react";
+import api from 'services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Desafio ReactJS",
+      url: "https://github.com/danilolutz",
+      techs: ["javascript", "typescript"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    if (response.status === 204) {
+      const refreshed = repositories.filter(repository => repository.id !== id);
+      setRepositories([...refreshed]);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
